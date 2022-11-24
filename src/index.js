d6b5313dc0af89b6163827389c978e5ffae5ef74
AlexFlowers <>
2022-11-24 07:56:20 +0300

    files are structured a bit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// // // IMPORTS
+// // modules
+import ReactDOM from 'react-dom'
+// // styles
+import './styles/overalls.scss'
+// // components
+import App from './scripts/components/App/App'
+
+// CODE
+ReactDOM.render(<App/>, document.querySelector('#root'))
