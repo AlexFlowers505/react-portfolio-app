342a1dc46ad48fe21e430b9d72f0e591eb9314b5
AlexFlowers505 <allnewalex@Yandex.com>
2022-12-02 20:48:21 +0300

    useState is not a fn issue not solved; nav was worked on

--- src/scripts/components/section-like/nav/Nav.js
+++ src/scripts/components/section-like/nav/Nav.js
@@ -4,20 +4,23 @@ import React from 'react'
 /* =============================== Hooks =============================== */
 import { useState } from 'react'
 /* =============================== Components =============================== */
+/* ============== insides ============== */
+import NavItem from './insides/NavItem'
 /* ============== icons ============== */
 import { AiOutlineHome as HomeIcon, AiOutlineUser as UserIcon } from 'react-icons/ai'
 import { BiBook as BookIcon, BiMessageSquareDetail as ChatIcon } from 'react-icons/bi'
 import { RiServiceLine as HeartShakeIcon } from 'react-icons/ri'
 
 export default function Nav() {
-  const [activeItem, setActiveItem] = useState('#')
+  const [activeItem, setActiveItem] = useState('')
+
   return (
     <nav className='main-nav'>
-      <a className={`main-nav__item ${activeItem === '#' ? 'main-nav__item--active' : ''}`} href='#'><HomeIcon /></a>
-      <a className='main-nav__item' href='#about-section'><BookIcon /></a>
-      <a className='main-nav__item' href='#xp-section'><UserIcon /></a>
-      <a className='main-nav__item' href='#services-section'><HeartShakeIcon /></a>
-      <a className='main-nav__item' href='#contacts-section'><ChatIcon /></a>
+      <NavItem id='#' icon={<HomeIcon />} activeToggler={setActiveItem} activeIndicator={activeItem}/>
+      <NavItem id='#about-section' icon={<BookIcon />} activeToggler={setActiveItem} activeIndicator={activeItem}/>
+      <NavItem id='#xp-section' icon={<UserIcon />} activeToggler={setActiveItem} activeIndicator={activeItem}/>
+      <NavItem id='#services-section' icon={<HeartShakeIcon />} activeToggler={setActiveItem} activeIndicator={activeItem}/>
+      <NavItem id='#contacts-section' icon={<ChatIcon />} activeToggler={setActiveItem} activeIndicator={activeItem}/>
     </nav>
   )
 }
