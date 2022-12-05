3db53939f534475a9be6c395ad00b9746acfe873
AlexFlowers <>
2022-12-05 10:05:05 +0300

    about section created n styled a bit

--- src/scripts/components/section-like/about/About.js
+++ src/scripts/components/section-like/about/About.js
@@ -1,7 +1,42 @@
 import React from 'react'
+import myPhoto from '../../../../assets/img/pics/me-photos/photo2.jpg'
+import { FaAward as AwardIcon } from 'react-icons/fa'
+import { FiUsers as UsersIcon } from 'react-icons/fi'
+import { VscFolderLibrary as DirIcon } from 'react-icons/vsc'
+
 
 export default function About() {
   return (
-    <section id='about-section'>About</section>
+    <section id='about-section'>
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+			<div className='tank about-section__contents-wrapper'>
+        <div className='about-section__img-outer-wrapper'>
+          <div className='about-section__img-inner-wrapper'>
+            <img className='about-section__img' src={myPhoto} alt="Alexander" />
+          </div>
+        </div>
+        <div className="about-section__text-content text-content">
+          <div className="text-content__cards">
+            <article className="text-content__about-card about-card">
+              <AwardIcon className='about-card__icon' />
+              <h5 className='about-card__heading'>Eperience</h5>
+              <small className='about-card__desc'>3+ Years Working</small>
+            </article>
+            <article className="text-content__about-card about-card">
+              <UsersIcon className='about-card__icon' />
+              <h5 className='about-card__heading'>Clients</h5>
+              <small className='about-card__desc'>200+</small>
+            </article>
+            <article className="text-content__about-card about-card">
+              <DirIcon className='about-card__icon' />
+              <h5 className='about-card__heading'>Projects</h5>
+              <small className='about-card__desc'>80+</small>
+            </article>
+          </div>
+          <a className='text-content__contact-cta btn btn-primary' href="#contacts-section">Let's talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
