a3af3359bd585d72c991cf34b16c84ebf51835c2
AlexFlowers <>
2022-12-05 22:41:53 +0300

    adjustment to the previous commit again

--- src/scripts/components/section-like/xp/Xp.js
+++ src/scripts/components/section-like/xp/Xp.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { BsPatchCheckFill as CheckIcon } from 'react-icons/bs'
+import { BsPatchCheckFill as StarCheckIcon } from 'react-icons/bs'
 
 export default function Xp() {
 	return (
@@ -12,42 +12,42 @@ export default function Xp() {
 					<h3 className='xp-card__heading'>Frontend Development</h3>
 					<div className='xp-card__contents'>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>HTML</h4>
 								<small className='xp-item__desc txt-light'>Experienced</small>
 							</div>
 						</div>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>CSS</h4>
 								<small className='xp-item__desc txt-light'>Intermediate</small>
 							</div>
 						</div>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>JavaScript</h4>
 								<small className='xp-item__desc txt-light'>Experienced</small>
 							</div>
 						</div>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>Bootstrap</h4>
 								<small className='xp-item__desc txt-light'>Experienced</small>
 							</div>
 						</div>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>Tailwind</h4>
 								<small className='xp-item__desc txt-light'>Experienced</small>
 							</div>
 						</div>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>React</h4>
 								<small className='xp-item__desc txt-light'>Experienced</small>
@@ -59,35 +59,35 @@ export default function Xp() {
 					<h3 className='xp-card__heading'>Backend Development</h3>
 					<div className='xp-card__contents '>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>Node JS</h4>
 								<small className='xp-item__desc txt-light'>Experienced</small>
 							</div>
 						</div>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>MongoDB</h4>
 								<small className='xp-item__desc txt-light'>Intermediate</small>
 							</div>
 						</div>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>PHP</h4>
 								<small className='xp-item__desc txt-light'>Experienced</small>
 							</div>
 						</div>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>MySQL</h4>
 								<small className='xp-item__desc txt-light'>Experienced</small>
 							</div>
 						</div>
 						<div className='xp-card__xp-item xp-item'>
-							<CheckIcon className='xp-item__icon' />
+							<StarCheckIcon className='xp-item__icon' />
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>Python</h4>
 								<small className='xp-item__desc txt-light'>Experienced</small>
