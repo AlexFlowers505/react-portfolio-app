127bd2baeb669c84b814405dacdf2e43874f913f
AlexFlowers <>
2022-12-08 08:04:28 +0300

    bem classes on services section

--- src/scripts/components/section-like/services/Services.js
+++ src/scripts/components/section-like/services/Services.js
@@ -6,92 +6,92 @@ export default function Services() {
     <section id='services-section' className='services-section'>
       <h5>What I Offer</h5>
       <h2>Services</h2>
-      <div className="services__container tank">
-        <article className="service services-section__service-card service-card">
-          <div className="service__head service-card__heading-wrapper">
+      <div className="services-section__inner-wrapper tank">
+        <article className="services-section__service-card service-card">
+          <div className="service-card__heading-wrapper">
             <h3 className='service-card__heading'>UI/UX Design</h3>
           </div>
-            <ul className='service-card__service-desc-list service-desc-list service__list'>
+            <ul className='service-card__service-desc-list service-desc-list'>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
             </ul>
         </article>
-        <article className="service services-section__service-card service-card">
-          <div className="service__head service-card__heading-wrapper">
+        <article className="services-section__service-card service-card">
+          <div className="service-card__heading-wrapper">
             <h3 className='service-card__heading'>Web Development</h3>
           </div>
-            <ul className='service-card__service-desc-list service-desc-list service__list'>
+            <ul className='service-card__service-desc-list service-desc-list'>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
             </ul>
         </article>
-        <article className="service services-section__service-card service-card">
-          <div className="service__head service-card__heading-wrapper">
+        <article className="services-section__service-card service-card">
+          <div className="service-card__heading-wrapper">
             <h3 className='service-card__heading'>Content Creation</h3>
           </div>
-            <ul className='service-card__service-desc-list service-desc-list service__list'>
+            <ul className='service-card__service-desc-list service-desc-list'>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
               <li className='service-desc-list__service-desc-item service-desc-item'>
-                <CheckIcon className='service__list-icon service-desc-item__icon' />
+                <CheckIcon className='service-desc-item__icon' />
                 <p className='service-desc-item__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium?</p>
               </li>
             </ul>
