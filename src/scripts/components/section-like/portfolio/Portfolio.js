d11ea25d94780f568f15783f7130badf0b680b87
AlexFlowers <>
2022-12-09 22:30:42 +0300

    ok classes used instead of weird ones

--- src/scripts/components/section-like/portfolio/Portfolio.js
+++ src/scripts/components/section-like/portfolio/Portfolio.js
@@ -50,16 +50,16 @@ export default function Portfolio() {
 		<section id='portfolio-section' className='portfolio-section'>
 			<h5 className='portfolio-section__preheading'>My Recent Work</h5>
 			<h2 className='portfolio-section__heading'>Portfolio</h2>
-			<div className="tank portfolio__container portfolio-section__content-wrapper content-wrapper">
+			<div className="tank portfolio-section__content-wrapper content-wrapper">
 				{
 					data.map(({ img, title, githubURL, demoURL }, i) => {
 						return (
-							<article className='portfolio__item content-wrapper__content-item content-item' key={i}>
-								<div className="portfolio__item-image content-item__img-wrapper">
+							<article className='content-wrapper__content-item content-item' key={i}>
+								<div className='content-item__img-wrapper'>
 									<img className='content-item__img' src={img} alt={title} />
 								</div>
 								<h3 className='content-item__heading'>{title}</h3>
-								<div className="portfolio__item-cta content-item__cta-wrapper">
+								<div className="content-item__cta-wrapper">
 									<a className='btn content-item__cta-btn' href={githubURL} target='_blank' rel="noopener noreferrer">Github</a>
 									<a className='btn btn-primary content-item__cta-btn' href={demoURL} target='_blank' rel="noopener noreferrer">Live Demo</a>
 								</div>
