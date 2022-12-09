99e4e8390e098d016487f16d604d0cca45745a72
AlexFlowers <>
2022-12-10 00:14:47 +0300

    testimonials ok classes name styling done, mapping done, data moved to the right dir, swiper to go

--- src/scripts/components/section-like/testimonials/Testimonials.js
+++ src/scripts/components/section-like/testimonials/Testimonials.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { testimonialsData as data } from '../../../data/testimonials/testimonialsData'
 
 export default function Testimonials() {
-  return (
-    <section id="testimonials-section">Testimonials</section>
-  )
+	return (
+		<section id='testimonials-section' className='testimonials-section'>
+			<h5 className='testimonials-section__preheading'>Review from clients</h5>
+			<h2 className='testimonials-section__heading'>Testimonals</h2>
+			<div className="tank testimonials-section__contents-wrapper contents-wrapper">
+				{data.map(({avatar, name, review}, i) => {
+					return (
+						<article className='contents-wrapper__review review' key={i}>
+							<div className='review__author-pic-wrapper'>
+								<img className='review__author-pic' src={avatar} alt={name} />
+							</div>
+							<h5 className='review__author-name'>Ernest Achiever</h5>
+							<small className='review__the-review'>{review}</small>
+						</article>
+					)
+				})}
+			</div>
+		</section>
+	)
 }
---
+++ src/scripts/data/testimonials/testimonialsData.js
@@ -0,0 +1,19 @@
+import Avt1 from '../../../assets/img/pics/testimonials-authors/avatar1.jpg'
+
+export const testimonialsData = [
+	{
+		avatar: Avt1,
+		name: 'Tina Snow',
+		review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, sunt.',
+	},
+	{
+		avatar: Avt1,
+		name: 'Tina Snow',
+		review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, sunt.',
+	},
+	{
+		avatar: Avt1,
+		name: 'Tina Snow',
+		review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, sunt.',
+	},
+]
