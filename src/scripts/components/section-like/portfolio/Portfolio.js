49a7197850ba4481b74e7c558b431999c1e900e7
AlexFlowers <>
2022-12-09 22:19:41 +0300

    portfolio section done with weird class names and some mapping fn

--- src/scripts/components/section-like/portfolio/Portfolio.js
+++ src/scripts/components/section-like/portfolio/Portfolio.js
@@ -6,20 +6,67 @@ import IMG4 from '../../../../assets/img/pics/portfolio-previews/dev/portfolio4.
 import IMG5 from '../../../../assets/img/pics/portfolio-previews/dev/portfolio5.png'
 import IMG6 from '../../../../assets/img/pics/portfolio-previews/dev/portfolio6.jpg'
 
+const data = [
+	{
+		img: IMG1,
+		title: 'Crypto Currency Dashboard & Financial Visualization',
+		githubURL: 'https://github.com',
+		demoURL: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization',
+	},
+	{
+		img: IMG1,
+		title: 'Crypto Currency Dashboard & Financial Visualization',
+		githubURL: 'https://github.com',
+		demoURL: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization',
+	},
+	{
+		img: IMG1,
+		title: 'Crypto Currency Dashboard & Financial Visualization',
+		githubURL: 'https://github.com',
+		demoURL: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization',
+	},
+	{
+		img: IMG1,
+		title: 'Crypto Currency Dashboard & Financial Visualization',
+		githubURL: 'https://github.com',
+		demoURL: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization',
+	},
+	{
+		img: IMG1,
+		title: 'Crypto Currency Dashboard & Financial Visualization',
+		githubURL: 'https://github.com',
+		demoURL: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization',
+	},
+	{
+		img: IMG1,
+		title: 'Crypto Currency Dashboard & Financial Visualization',
+		githubURL: 'https://github.com',
+		demoURL: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization',
+	},
+]
+
 export default function Portfolio() {
 	return (
 		<section id='portfolio-section' className='portfolio-section'>
 			<h5 className='portfolio-section__preheading'>My Recent Work</h5>
 			<h2 className='portfolio-section__heading'>Portfolio</h2>
 			<div className="tank portfolio__container portfolio-section__content-wrapper content-wrapper">
-				<article className='portfolio__item content-wrapper__content-item content-item'>
-					<div className="portfolio__item-image content-item__img-wrapper">
-						<img className='content-item__img' src={IMG1} alt="" />
-					</div>
-					<h3 className='content-item__heading'>This is a portfolio item title</h3>
-					<a className='btn content-item__btn' href="https://github.com">Github</a>
-					<a className='btn btn-primary content-item__btn' href="https://dribbble.com/shots/19771230-Sidebar-navigation-Untitled-UI" target='_blank' rel="noopener noreferrer">Live Demo</a>
-				</article>
+				{
+					data.map(({ img, title, githubURL, demoURL }, i) => {
+						return (
+							<article className='portfolio__item content-wrapper__content-item content-item' key={i}>
+								<div className="portfolio__item-image content-item__img-wrapper">
+									<img className='content-item__img' src={img} alt={title} />
+								</div>
+								<h3 className='content-item__heading'>{title}</h3>
+								<div className="portfolio__item-cta content-item__cta-wrapper">
+									<a className='btn content-item__cta-btn' href={githubURL} target='_blank' rel="noopener noreferrer">Github</a>
+									<a className='btn btn-primary content-item__cta-btn' href={demoURL} target='_blank' rel="noopener noreferrer">Live Demo</a>
+								</div>
+							</article>
+						)
+					})
+				}
 			</div>
 		</section>
 	)
