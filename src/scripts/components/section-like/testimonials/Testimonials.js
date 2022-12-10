3c77717e0dfa0c32bf78908360bd2452a40c34f5
AlexFlowers <>
2022-12-11 00:39:24 +0300

    - testimonials done

--- src/scripts/components/section-like/testimonials/Testimonials.js
+++ src/scripts/components/section-like/testimonials/Testimonials.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import { testimonialsData as data } from '../../../data/testimonials/testimonialsData'
 
+import { Pagination, Navigation } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+
 export default function Testimonials() {
 	return (
 		<section id='testimonials-section' className='testimonials-section'>
 			<h5 className='testimonials-section__preheading'>Review from clients</h5>
 			<h2 className='testimonials-section__heading'>Testimonals</h2>
-			<div className="tank testimonials-section__contents-wrapper contents-wrapper">
+			<Swiper 
+				className="tank testimonials-section__contents-wrapper contents-wrapper"
+				modules={[ Pagination ]}
+				spaceBetween={40}
+				slidesPerView={1}
+				pagination={{ clickable: true }}
+				navigation
+			>
 				{data.map(({avatar, name, review}, i) => {
 					return (
-						<article className='contents-wrapper__review review' key={i}>
+						<SwiperSlide className='contents-wrapper__review review' key={i}>
 							<div className='review__author-pic-wrapper'>
 								<img className='review__author-pic' src={avatar} alt={name} />
 							</div>
 							<h5 className='review__author-name'>Ernest Achiever</h5>
 							<small className='review__the-review'>{review}</small>
-						</article>
+						</SwiperSlide>
 					)
 				})}
-			</div>
+			</Swiper>
 		</section>
 	)
 }
