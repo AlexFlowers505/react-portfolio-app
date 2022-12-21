a822d289523c1d9b708a65c6e7966af355001fed
AlexFlowers <>
2022-12-21 08:16:15 +0300

    weird class names footer layout n styling n responsive styling r done

--- src/scripts/components/section-like/contacts/Contacts.js
+++ src/scripts/components/section-like/contacts/Contacts.js
@@ -6,34 +6,6 @@ import { BsWhatsapp as WhatsappIcon} from 'react-icons/bs'
 
 import emailjs from 'emailjs-com'
 
-export const ContactUs = () => {
-  const form = useRef();
-
-  const sendEmail = evt => {
-    evt.preventDefault()
-
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-      .then( result => {
-          console.log(result.text)
-      }, err => {
-          console.log(err.text)
-      })
-  }
-
-  return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
-};
-
-
 export default function Contacts() {
 	const form = useRef()
 	const sendEmail = evt => {
--- src/scripts/components/section-like/footer/Footer.js
+++ src/scripts/components/section-like/footer/Footer.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { RiMessengerLine as FBIcon} from 'react-icons/ri'
+import { FiInstagram as InstaIcon} from 'react-icons/fi'
+import { IoLogoTwitter as TwitterIcon} from 'react-icons/io'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer id='footer' className='footer'>
+      <a href='' className='footer__logo logo'></a>
+      <ul className='footer__links-list permalinks'>
+        <li className='footer__links-item'><a href='#' className='footer__the-link'>Home</a></li>
+        <li className='footer__links-item'><a href='#about-section' className='footer__the-link'>About</a></li>
+        <li className='footer__links-item'><a href='#xp-section' className='footer__the-link'>Experience</a></li>
+        <li className='footer__links-item'><a href='#services-section' className='footer__the-link'>Services</a></li>
+        <li className='footer__links-item'><a href='#portfolio-section' className='footer__the-link'>Portfolio</a></li>
+        <li className='footer__links-item'><a href='#testimonials-section' className='footer__the-link'>Testimonials</a></li>
+        <li className='footer__links-item'><a href='#contacts-section' className='footer__the-link'>Contacts</a></li>
+      </ul>
+      <ul className="footer__socials-list footer__socials">
+        <li className="footer__socials-item"><a href="" className="footer__socials-the-link"><InstaIcon /></a></li>
+        <li className="footer__socials-item"><a href="" className="footer__socials-the-link"><FBIcon /></a></li>
+        <li className="footer__socials-item"><a href="" className="footer__socials-the-link"><TwitterIcon /></a></li>
+      </ul>
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
