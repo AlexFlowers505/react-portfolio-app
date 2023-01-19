a3eacc29e3d885168e2528c63462759cd91cde15
AlexFlowers505 <allnewalex@Yandex.com>
2023-01-19 12:31:14 +0300

    reviews authors avatars solved

--- src/scripts/data/testimonials/testimonialsData.js
+++ src/scripts/data/testimonials/testimonialsData.js
@@ -1,19 +1,21 @@
-import Avt1 from '../../../assets/img/pics/testimonials-authors/avatar1.jpg'
+import AlexJones from '../../../assets/img/pics/testimonials-authors/AlexJones.png'
+import HarrisFerguson from '../../../assets/img/pics/testimonials-authors/HarrisFerguson.png'
+import MartinKelly from '../../../assets/img/pics/testimonials-authors/MartinKelly.png'
 import AleksandraBojarska from '../../../assets/img/pics/testimonials-authors/AleksandraBojarska.png'
 
 export const testimonialsData = [
 	{
-		avatar: Avt1,
-		name: 'Martin Foster, Melbourne Australia',
+		avatar: MartinKelly,
+		name: 'Martin Kelly, Melbourne Australia',
 		review: 'Alex was fantastic, Worked with me very well to get a clear understanding of what I wanted. Even provided me options to different designs so I could choose my favourite. Highly recommended.',
 	},
 	{
-		avatar: Avt1,
+		avatar: HarrisFerguson,
 		name: 'Harris Ferguson, Brisbane Australia',
 		review: 'Alexander is an enthusiastic and skilled addition to upworks. Give him good direction, he will talk with you and he will produce some very good work.',
 	},
 	{
-		avatar: Avt1,
+		avatar: AlexJones,
 		name: 'Alex Jones, Blackbutt Australia',
 		review: 'Great person to work with. Understood everything I needed. His work was great.',
 	},
