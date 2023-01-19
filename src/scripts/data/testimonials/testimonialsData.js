855786d4c3f72228c6753b636e0b79f8108da9ca
AlexFlowers505 <allnewalex@Yandex.com>
2023-01-19 12:24:08 +0300

    reviews names added, avatars to go

--- src/scripts/data/testimonials/testimonialsData.js
+++ src/scripts/data/testimonials/testimonialsData.js
@@ -4,22 +4,22 @@ import AleksandraBojarska from '../../../assets/img/pics/testimonials-authors/Al
 export const testimonialsData = [
 	{
 		avatar: Avt1,
-		name: 'Tina Snow',
+		name: 'Martin Foster, Melbourne Australia',
 		review: 'Alex was fantastic, Worked with me very well to get a clear understanding of what I wanted. Even provided me options to different designs so I could choose my favourite. Highly recommended.',
 	},
 	{
 		avatar: Avt1,
-		name: 'Tina Snow',
+		name: 'Harris Ferguson, Brisbane Australia',
 		review: 'Alexander is an enthusiastic and skilled addition to upworks. Give him good direction, he will talk with you and he will produce some very good work.',
 	},
 	{
 		avatar: Avt1,
-		name: 'Tina Snow',
+		name: 'Alex Jones, Blackbutt Australia',
 		review: 'Great person to work with. Understood everything I needed. His work was great.',
 	},
 	{
 		avatar: AleksandraBojarska,
-		name: 'Aleksandra Bojarska, Poland',
+		name: 'Aleksandra Bojarska, Gdansk Poland',
 		review: 'It is the second time I have worked with Alexander, and as the first time, I enjoyed the work and the results very much. I reccomend Alexander and I definitely keep working with him on future projects.',
 	},
 ]
