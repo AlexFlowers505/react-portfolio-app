d23222675f3b8d28822dffdf6ea9dc51cdad7cdc
AlexFlowers <allnewalex@yandex.com>
2025-03-01 11:04:18 +0300

    stroke-width changed to strokeWitdth

--- src/scripts/components/oth/SvgSprite.jsx
+++ src/scripts/components/oth/SvgSprite.jsx
@@ -38,7 +38,7 @@ export default function SvgSprite() {
           id="star"
           stroke="currentColor"
           fill="currentColor"
-          stroke-width="0"
+          strokeWidth="0"
           viewBox="0 0 576 512"
           xmlns="http://www.w3.org/2000/svg"
         >
