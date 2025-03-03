8797f258c5741cfc937e695f0d3c008d96f57d3d
AlexFlowers <allnewalex@yandex.com>
2025-03-03 08:35:35 +0300

    xp section data adjusted

--- src/scripts/components/section-like/xp/Xp.js
+++ src/scripts/components/section-like/xp/Xp.js
@@ -8,6 +8,79 @@ export default function Xp() {
 			<h5>Подробнее о навыках</h5>
 			<h2>Мой опыт</h2>
 			<div className='tank xp-section__contents-wrapper'>
+			<article className='xp-section__xp-card xp-card'>
+					<h3 className='xp-card__heading'>Фронтенд разработка</h3>
+					<div className='xp-card__contents '>
+						<div className='xp-card__xp-item xp-item xp-item--js'>
+							<ProgressCircle />
+							<div className='xp-item__data-wrapper'>
+								<h4 className='xp-item__heading'>Vanilla JS</h4>
+								<small className='xp-item__desc txt-light'>★★★★</small>
+							</div>
+						</div>
+						<div className='xp-card__xp-item xp-item xp-item--react'>
+							<ProgressCircle />
+							<div className='xp-item__data-wrapper'>
+								<h4 className='xp-item__heading'>React</h4>
+								<small className='xp-item__desc txt-light'>★★★★</small>
+							</div>
+						</div>
+						<div className='xp-card__xp-item xp-item xp-item--typescript'>
+							<ProgressCircle />
+							<div className='xp-item__data-wrapper'>
+								<h4 className='xp-item__heading'>TypeScript</h4>
+								<small className='xp-item__desc txt-light'>★★★★</small>
+							</div>
+						</div>
+						<div className='xp-card__xp-item xp-item xp-item--wb'>
+							<ProgressCircle />
+							<div className='xp-item__data-wrapper'>
+								<h4 className='xp-item__heading'>WebPack</h4>
+								<small className='xp-item__desc txt-light'>★★</small>
+							</div>
+						</div>
+					</div>
+				</article>
+				<article className='xp-section__xp-card xp-card'>
+					<h3 className='xp-card__heading'>Верстка+</h3>
+					<div className='xp-card__contents'>
+						<div className='xp-card__xp-item xp-item xp-item--html'>
+							<ProgressCircle />
+							<div className='xp-item__data-wrapper'>
+								<h4 className='xp-item__heading'>HTML</h4>
+								<small className='xp-item__desc txt-light'>★★★★★</small>
+							</div>
+						</div>
+						<div className='xp-card__xp-item xp-item xp-item--sass'>
+							<ProgressCircle />
+							<div className='xp-item__data-wrapper'>
+								<h4 className='xp-item__heading'>Sass</h4>
+								<small className='xp-item__desc txt-light'>★★★★★</small>
+							</div>
+						</div>
+						<div className='xp-card__xp-item xp-item xp-item--tailwind-css'>
+							<ProgressCircle />
+							<div className='xp-item__data-wrapper'>
+								<h4 className='xp-item__heading'>Tailwind CSS</h4>
+								<small className='xp-item__desc txt-light'>★★★★★</small>
+							</div>
+						</div>
+						<div className='xp-card__xp-item xp-item xp-item--bt'>
+							<ProgressCircle />
+							<div className='xp-item__data-wrapper'>
+								<h4 className='xp-item__heading'>Bootstrap</h4>
+								<small className='xp-item__desc txt-light'>★★★★</small>
+							</div>
+						</div>
+						<div className='xp-card__xp-item xp-item xp-item--bem'>
+							<ProgressCircle />
+							<div className='xp-item__data-wrapper'>
+								<h4 className='xp-item__heading'>BEM</h4>
+								<small className='xp-item__desc txt-light'>★★★★★</small>
+							</div>
+						</div>
+					</div>
+				</article>
 				<article className='xp-section__xp-card xp-card'>
 					<h3 className='xp-card__heading'>UX/UI дизайн</h3>
 					<div className='xp-card__contents'>
@@ -44,79 +117,6 @@ export default function Xp() {
 							<div className='xp-item__data-wrapper'>
 								<h4 className='xp-item__heading'>Adobe Premiere Pro</h4>
 								<small className='xp-item__desc txt-light'>★★★</small>
-							</div>
-						</div>
-					</div>
-				</article>
-				<article className='xp-section__xp-card xp-card'>
-					<h3 className='xp-card__heading'>Верстка+</h3>
-					<div className='xp-card__contents'>
-						<div className='xp-card__xp-item xp-item xp-item--html'>
-							<ProgressCircle />
-							<div className='xp-item__data-wrapper'>
-								<h4 className='xp-item__heading'>HTML</h4>
-								<small className='xp-item__desc txt-light'>★★★★★</small>
-							</div>
-						</div>
-						<div className='xp-card__xp-item xp-item xp-item--sass'>
-							<ProgressCircle />
-							<div className='xp-item__data-wrapper'>
-								<h4 className='xp-item__heading'>Sass</h4>
-								<small className='xp-item__desc txt-light'>★★★★★</small>
-							</div>
-						</div>
-						<div className='xp-card__xp-item xp-item xp-item--bt'>
-							<ProgressCircle />
-							<div className='xp-item__data-wrapper'>
-								<h4 className='xp-item__heading'>Bootstrap</h4>
-								<small className='xp-item__desc txt-light'>★★★★</small>
-							</div>
-						</div>
-						<div className='xp-card__xp-item xp-item xp-item--bem'>
-							<ProgressCircle />
-							<div className='xp-item__data-wrapper'>
-								<h4 className='xp-item__heading'>BEM</h4>
-								<small className='xp-item__desc txt-light'>★★★★★</small>
-							</div>
-						</div>
-						<div className='xp-card__xp-item xp-item xp-item--ant'>
-							<ProgressCircle />
-							<div className='xp-item__data-wrapper'>
-								<h4 className='xp-item__heading'>AntDesign</h4>
-								<small className='xp-item__desc txt-light'>★★★</small>
-							</div>
-						</div>
-						<div className='xp-card__xp-item xp-item xp-item--git'>
-							<ProgressCircle />
-							<div className='xp-item__data-wrapper'>
-								<h4 className='xp-item__heading'>Git & GitLab & Github</h4>
-								<small className='xp-item__desc txt-light'>★★★★</small>
-							</div>
-						</div>
-					</div>
-				</article>
-				<article className='xp-section__xp-card xp-card'>
-					<h3 className='xp-card__heading'>Фронтенд разработка</h3>
-					<div className='xp-card__contents '>
-						<div className='xp-card__xp-item xp-item xp-item--js'>
-							<ProgressCircle />
-							<div className='xp-item__data-wrapper'>
-								<h4 className='xp-item__heading'>Vanilla JS</h4>
-								<small className='xp-item__desc txt-light'>★★★★</small>
-							</div>
-						</div>
-						<div className='xp-card__xp-item xp-item xp-item--react'>
-							<ProgressCircle />
-							<div className='xp-item__data-wrapper'>
-								<h4 className='xp-item__heading'>React</h4>
-								<small className='xp-item__desc txt-light'>★★★★</small>
-							</div>
-						</div>
-						<div className='xp-card__xp-item xp-item xp-item--wb'>
-							<ProgressCircle />
-							<div className='xp-item__data-wrapper'>
-								<h4 className='xp-item__heading'>WebPack</h4>
-								<small className='xp-item__desc txt-light'>★★</small>
 							</div>
 						</div>
 					</div>
